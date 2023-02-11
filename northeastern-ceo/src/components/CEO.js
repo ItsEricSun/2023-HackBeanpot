f5c42931ed501baf08e1336330f420799e39ea88
Anzhuo-W <45926251+Anzhuo-W@users.noreply.github.com>
2023-02-11 15:46:46 -0500

    edited functions

--- northeastern-ceo/src/components/CEO.js
+++ northeastern-ceo/src/components/CEO.js
@@ -3,7 +3,14 @@ import React from "react"
 class CEO extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { balance: 1, displayBalance: 1.0, students: 0, displayStudents: 0.0, seconds: 0, displaySeconds: 0.0};
+        this.state = { 
+            balance: 1, 
+            displayBalance: 1.0, 
+            students: 0, 
+            displayStudents: 0.0, 
+            seconds: 0, 
+            displaySeconds: 0.0,
+            displayStudentCost: 1.0};
         //   this.handleChange = this.handleChange.bind(this);
         this.handleMakeMoney = this.handleMakeMoney.bind(this);
         this.handleBribeChild = this.handleBribeChild.bind(this);
@@ -18,17 +25,22 @@ class CEO extends React.Component {
                 <p>{this.state.displaySeconds}</p>
                 <form onSubmit={this.handleMakeMoney}>
                     <button>
-                        Make Money!!
+                        Make Money
                     </button>
                 </form>
+
+
                 <form onSubmit={this.handleBribeChild}>
                     <button>
-                        Bribe a Child (+ 1 student). Cost $5
+                        Bribe a Child
                     </button>
                 </form>
+                <p>Cost: {this.state.displayStudentCost}</p>
+
+
                 <form onSubmit={this.handleMarketing}>
                     <button>
-                        Make a marketing video (+ 0-4 students). Cost $12
+                        Create marketing video
                     </button>
                 </form>
             </div>
@@ -37,27 +49,54 @@ class CEO extends React.Component {
 
     handleMakeMoney(e) {
         e.preventDefault();
+        let expo = this.state.displayStudents * 0.15;
+        if (this.state.balance < 20) {
+            this.setState(state => ({
+                balance: state.balance + 1,
+                displayBalance : parseInt(state.balance + 1)
+            }));
+        } else {
+            this.setState(state => ({
+                balance: state.balance * Math.floor(expo),
+                displayBalance : parseInt(state.balance + Math.floor(expo))
+            }));
+        }
+        if (this.state.displayStudentCost < 10) {
+            this.setState(state => ({
+                displayStudentCost: state.displayStudentCost + 1
+            }))
+            return;
+        }
+        let multiplier = Math.random() / 16;
+        let newCost = Math.floor(Math.pow(this.state.displayStudentCost, 1 + multiplier));
         this.setState(state => ({
-            balance: state.balance + 1,
-            displayBalance : parseInt(state.balance + 1)
+            displayStudentCost: parseInt(newCost)
         }));
-        
-
     }
 
     handleBribeChild(e) {
         e.preventDefault();
-        if (this.state.balance < 5) {
+        if (this.state.balance < this.state.displayStudentCost) {
             return;
         }
         let newStudents = 1;
         this.setState(state => ({
-            balance: state.balance - 5,
+            balance: state.balance - state.displayStudentCost,
             students: state.students + newStudents,
-            displayBalance : parseInt(state.balance - 5),
-            displayStudents : parseInt(state.students + newStudents)
-            
+            displayBalance : parseInt(state.balance - state.displayStudentCost),
+            displayStudents : parseInt(state.students + newStudents),
         }));
+        if (this.state.displayStudentCost < 10) {
+            this.setState(state => ({
+                displayStudentCost: state.displayStudentCost + 1
+            }))
+        } else {
+            let multiplier = Math.random() * 2 / 16;
+            let newCost = Math.floor(Math.pow(this.state.displayStudentCost, 1 + multiplier));
+            this.setState(state => ({
+                displayStudentCost: parseInt(newCost)
+            }));
+        }
     }
 
     handleMarketing(e) {
