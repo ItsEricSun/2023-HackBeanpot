c8a3a45ea50f7b7bd9e9985feade4ec563a60cc0
Chris-N-Wu <dolphins0248@gmail.com>
2023-02-12 03:32:55 -0500

    1

--- northeastern-ceo/src/components/President.js
+++ northeastern-ceo/src/components/President.js
@@ -5,7 +5,7 @@ function President({ name }) {
         <div className='name1'>
             <div className='universityName'>
                 <h1 className='welcome'>Welcome, </h1>
-                <h1 className='ceoName'>Northeastern University CEO {name}</h1>
+                <h1 className='ceoName'>Northeastern University CEO, {name}</h1>
             </div>
         </div>
     )
